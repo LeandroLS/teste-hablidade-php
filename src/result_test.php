0516a21d6bfd72bac0a86424845775c37b6cb19e
Leandro <llimas@outlook.com>
2018-05-28 19:36:20 -0300

    mostrando grafico no resultado das questões

--- src/result_test.php
+++ src/result_test.php
@@ -11,6 +11,9 @@ $questions = $connection->query($questionsQuery);
 $questions->execute();
 $questions = $questions->fetchAll(); 
 
+$numCorrectAnswer = 0;
+$numAswners = 20;
+
 ?>
 
 <!DOCTYPE html>
@@ -20,10 +23,14 @@ $questions = $questions->fetchAll();
         <title>Teste de Habilidade PHP</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
     </head>
     <body>
         <div class="container">
             <h1 class="text-primary text-center">Resultado do Questionário</h1>
+            <div style="height:400px">
+                <canvas id="myChart"></canvas>
+            </div>
             <form action="result_test.php" method="post">
             <?php foreach($questions as $key => $question): ?>
                 <fieldset class="border" style="margin-bottom:10px">
@@ -51,7 +58,7 @@ $questions = $questions->fetchAll();
                                 <?php if($answer['question_id'] == $question['question_id'] && $answer['correct_answer'] == 1): ?>
                                     <tr>
                                         <td>
-                                            Resposta Correta: <?= utf8_encode($answer['answer_description']) ?>
+                                            <b>Resposta Correta:</b> <?= utf8_encode($answer['answer_description']) ?>
                                         </td>
                                     </tr>
                                 <?php endif ?>
@@ -62,8 +69,12 @@ $questions = $questions->fetchAll();
                                         <?php if($resposta == $answer['answer_id']): ?>
                                             <tr>
                                                 <td>
-                                                    Sua Resposta: <?= utf8_encode($answer['answer_description']) ?>
-                                                    <?= $answer['correct_answer'] == 1 ? "<i class='text-success fas fa-check-circle'></i>" : "<i class='text-danger fas fa-times-circle'></i>" ?> 
+                                                    <b>Sua Resposta:</b> <?= utf8_encode($answer['answer_description']) ?>
+                                                    <?php if($answer['correct_answer'] == 1): $numCorrectAnswer++?>
+                                                        <i class='text-success fas fa-check-circle'></i>
+                                                    <?php else:?>  
+                                                        <i class='text-danger fas fa-times-circle'></i>
+                                                    <?php endif ?> 
                                                 </td>
                                             </tr>
                                         <?php endif ?>
@@ -79,4 +90,26 @@ $questions = $questions->fetchAll();
             </form>
         </div>
     </body>
+   
+    <?php $charWrongAnswer = $numAswners - $numCorrectAnswer ?>
+
 </html>
+<script>
+    var ctx = document.getElementById("myChart");
+    var myChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: ["Corretas", "Erradas"],
+            datasets: [{
+                data: [<?= $numCorrectAnswer ?>, <?= $charWrongAnswer ?>],
+                backgroundColor: [
+                    'rgba(0, 168, 11, 0.5)',
+                    'rgba(255,0,0, 0.5)'
+                ]
+            }]
+        },
+        options: {
+            maintainAspectRatio: false,
+        }
+    });
+</script>
