618379315651a8f6ada4096505393b1860613bfc
Leandro Lima <desenvolvedor02@adias.com.br>
2019-11-01 16:43:32 -0300

    tentando dockerizar

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM php:7.2-apache
 EXPOSE 80
-RUN docker-php-ext-install pdo pdo_mysql
+RUN docker-php-ext-install pdo_mysql pdo
 COPY . /var/www/html
--- public/connection.php
+++ public/connection.php
@@ -3,11 +3,10 @@
 $username = "root";
 $password = "123456";
 $database = "teste_habilidade_php";
-$host = "localhost";
-$driver = "mysql";
+$host = "mysql";
 
 try {
-    $connection = new PDO("{$driver}:host={$host}:3307;dbname={$database}", $username, $password);
+    $connection = new PDO("mysql:host={$host};dbname={$database}", $username, $password);
 } catch(Exception $e){
     echo "Não foi possível conectar ao banco de dados, verifique as informações de conexão no arquivo " . getcwd() . "/connection.php";
     echo "<pre>"; echo $e; echo "</pre>";
