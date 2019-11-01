59f3ed4d146e44b43bc73eb34c4d5d8a8d8bb51d
Leandro Lima <llimas@outlook.com>
2019-11-01 00:18:38 -0300

    tentando dockerizar

--- public/connection.php
+++ public/connection.php
@@ -3,11 +3,11 @@
 $username = "root";
 $password = "123456";
 $database = "teste_habilidade_php";
-$host = "0.0.0.0";
+$host = "localhost";
 $driver = "mysql";
 
 try {
-    $connection = new PDO("{$driver}:dbname={$database};host={$host}:3307", $username, $password);
+    $connection = new PDO("{$driver}:host={$host}:3307;dbname={$database}", $username, $password);
 } catch(Exception $e){
     echo "Não foi possível conectar ao banco de dados, verifique as informações de conexão no arquivo " . getcwd() . "/connection.php";
     echo "<pre>"; echo $e; echo "</pre>";
